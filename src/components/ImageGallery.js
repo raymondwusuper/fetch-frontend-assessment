1776dbded1a267355626304a64a44468d3ba50e7
raymondwusuper <raymond.wu.super@gmail.com>
2024-09-26 16:06:05 -0700

    Update ImageGallery.js

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -15,27 +15,16 @@ const ImageGallery = ({ breedId }) => {
   }, [breedId]);
 
   return (
-    <div className="container mt-4">
-      <div className="row">
-        {images.length > 0 ? (
-          images.map((image) => (
-            <div className="col-sm-6 col-md-4 col-lg-3 mb-4" key={image.id}>
-              <div className="card">
-                <img
-                  src={image.url}
-                  alt="dog"
-                  className="card-img-top"
-                  style={{ height: '200px', objectFit: 'cover' }}
-                />
-              </div>
-            </div>
-          ))
-        ) : (
-          <div className="col-12">
-            <p className="text-center">No images available for this breed.</p>
+    <div className="row">
+      {images.length > 0 ? (
+        images.map((image) => (
+          <div className="col-md-4 col-sm-6 mb-4" key={image.id}>
+            <img src={image.url} alt="dog" className="img-fluid dog-image" />
           </div>
-        )}
-      </div>
+        ))
+      ) : (
+        <p>No images available for this breed.</p>
+      )}
     </div>
   );
 };
